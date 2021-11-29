0ee6c68d5850b2fd35afd5a6cb20d34e593d25b0
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-11-29 12:28:25 +0100

    EventListener / Tests

--- events.js
+++ events.js
@@ -1,10 +1,40 @@
 
 /********  Variablen **********/
-
-
+ const btn = document.getElementById("trigBtn");
+// const btn = document.body.children[0];                  //Damit macht die Überschrift dass was vorher der Button gemacht hat, wenn er der obere Code deaktiviert ist
 
 /********  Event-Listener **********/
+btn.addEventListener("click", calculate);
+btn.addEventListener("mouseenter", onRollIn);
+btn.addEventListener("mouseenter", onRollout);
 
+
+function test()
+{
+    output("Hi");
+}
+
+function onRollIn()
+{
+    output("onRollIn");
+}
+
+function onRollout()
+{
+    output("onRollout");
+}
+
+function calculate()
+{
+
+const one = parseInt(prompt("Bitte Zahl eingeben:"));
+const two = parseInt(prompt("Bitte Zahl eingeben:"));
+
+const three = one + two;
+
+output(three);
+    
+}
 
 
 /********  Business-Logic | Toggle **********/
@@ -13,7 +43,7 @@
 
 /********  Änderung in View-Schicht **********/
 
-/
+
 
 /* Tools */
 function output(outputData) {
